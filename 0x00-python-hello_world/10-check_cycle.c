6f6c5465900581b9105b926673efefe91e983548
OrangeB0lt <667@holbertonschool.com>
2019-04-29 22:04:26 -0400

    Upload first version of cycle checking

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list comming in
+ * Return: 1 if it cycles, 0 else
+ **/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *pointer = list, *checker = pointer->next;
+
+	if (list == NULL || list->next == NULL)
+	{
+		return (0);
+	}
+
+	while (pointer != NULL &&
+	       checker->next != NULL &&
+	       checker->next->next != NULL)
+	{
+		if (pointer == checker)
+		{
+			return (1);
+		}
+		pointer = pointer->next;
+		checker = checker->next->next;
+	}
+	return (0);
+}
