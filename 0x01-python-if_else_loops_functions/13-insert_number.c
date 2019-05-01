cee086a2b59f56fbe6b06dcc24246f8aab8c9fca
OrangeB0lt <667@holbertonschool.com>
2019-04-30 21:30:33 -0400

    Add a num to a sorted list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts a num into a sort list
+ * @head: pointer to list
+ * @number: num to add
+ * Return: address of the added node otherwise it adds NULL
+ **/
+
+listint_t *insert_node(;istint_t **head, int number)
+{
+	listint_t *last, *nexxt, *curr;
+
+	if (head == NULL)
+		return (NULL);
+	nexxt = malloc(sizeof(listint_t));
+	if (nexxt == NULL)
+		return (NULL);
+	nexxt->n = number;
+	last = NULL;
+	curr = *head;
+
+	for (; curr != NULL && curr->n < number;)
+	{
+		last = curr;
+		curr = curr->next;
+	}
+	nexxt->next = curr;
+
+	if (last != NULL)
+		last->next = nexxt;
+	else
+		*head = nexxt;
+	return (nexxt);
+}
