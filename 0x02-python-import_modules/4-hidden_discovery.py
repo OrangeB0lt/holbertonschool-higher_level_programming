c952ffb983e183eb0cc08c97e0f6b2097fd236fe
OrangeB0lt <667@holbertonschool.com>
2019-05-02 19:10:39 -0400

    Upload program toprint one name per line in hidden files

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3.4
 import hidden_4
-if __name__ == '__main__':
+if __name__ == "__main__":
     for string in dir(hidden_4):
         if string[:2] != "__":
-            print("{}" .format(string)
+            print("{}" .format(string))
