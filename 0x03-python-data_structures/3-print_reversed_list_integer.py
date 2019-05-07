ee48953f6e412488783a6785d180d07f04a86523
OrangeB0lt <667@holbertonschool.com>
2019-05-07 16:32:01 -0400

    upload 3 and 4

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        pass
+    else:
+        for index in reversed(range 0, len(my_list))):
+            print('{:d}' .form(my_list[index]))
---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    nu_lyst = list(my_list)
+    if idx < 0 or idx > len(my_list) - 1:
+        return nu_lyst
+    else:
+        nu_lyst[idx] = element
+        return nu_list
