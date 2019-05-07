ef71c84d8bb93e3acb1511c45f3515a4a9adf5dd
OrangeB0lt <667@holbertonschool.com>
2019-05-07 18:40:23 -0400

    Palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if list is a palindrome
+ * @head: start of list
+ * Return: returns 1 on success, 0 otherwise
+ **/
+
+int is palindrom(listint_t **head)
+{
+	listint_t *beginning;
+	int *storage, index = 0, length = 0;
+
+	beginning = *head;
+
+	while (start != NULL)
+		start = start->next, length++;
+	storage = malloc (sizeof(int) * (len));
+	if (storage == NULL)
+		return (0);
+	index = 0;
+	start = *head;
+	while (storage[index] == storage[length])
+	{
+		if (index == length || index > length)
+		{
+			free(storage);
+			return (1);
+		}
+		index++;
+		length--;
+	}
+	free(storage);
+	return(0);
+}
