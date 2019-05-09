ac4d95db9dff5977f1bcab2081aaa191de1ad51d
OrangeB0lt <667@holbertonschool.com>
2019-05-09 17:20:27 -0400

    Squars all ints in matrix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda hori: list(map(lambda num: num * num), hori)), matrix)
+    return list(map(lambda hori: list(map(lambda num: num ** 2, hori)), matrix))
