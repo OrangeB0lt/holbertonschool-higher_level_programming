d74f60feef7f5cdb6cfb320ab5c5771df2be7dcd
OrangeB0lt <667@holbertonschool.com>
2019-05-09 15:46:30 -0400

    Multiplies keys by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return {key: variable * 2 for key, variable in a_dictionary.items9()}
+    return {key: variable * 2 for key, variable in a_dictionary.items()}
