a470b534816952cf7a3df3a1474eda15a39a25e7
OrangeB0lt <667@holbertonschool.com>
2019-05-20 16:26:28 -0400

    safe print err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,8 +4,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return True
     except (ValueError, TypeError) as fail:
         stderr.write("Exception: {}\n".format(fail))
         return False
-    
+    return True
