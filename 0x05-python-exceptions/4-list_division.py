61c17d93f8db740b975b44386f20085396ddadd8
OrangeB0lt <667@holbertonschool.com>
2019-05-20 16:08:52 -0400

    4 divition printing

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+def list_division(my_list_1, my_list_2, list_length):
+    idx = 0
+    new_list = []
+    for idx in range(0, list_length):
+        try:
+            ans = my_list_1[idx] / my_list_2[idx]
+        except ZeroDivisionError:
+            ans = 0
+            print("division by 0")
+        except TypeError:
+            ans = 0
+            print("wrong type")
+        except IndexError:
+            ans = 0
+            print("out of range")
+        finally:
+            new_list.append(ans)
+    return new_list
