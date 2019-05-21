60f95bbbef01b23b3839c56010595bc7d956d0a6
OrangeB0lt <667@holbertonschool.com>
2019-05-21 19:33:58 -0400

    Checker updates

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,6 +47,34 @@ class Square:
         else:
             self.__size = value
 
+    @property
+    def position(self):
+        """position: give position of the square"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Potion: defines position setter value
+        """
+        if self._tuple(value):
+            self.__position = value
+        elif not self._tuple(value):
+            raise TypeError("Position must be a tuple of positive integers")
+
+    def _tuple(self, position):
+        """
+        check if it is a tuple and integer
+        """
+        if type(position) is not tuple or len(position) != 2:
+            return False
+        elif type(position[0]) is not int or position[0] < 0:
+            return False
+        elif type(position[1]) is not int or position[1] < 0:
+            return False
+        else:
+            return True
+
+
     def my_print(self):
         """
         prints to the stdout square with # or empty line if 0
@@ -59,16 +87,3 @@ class Square:
         Returns area of the square instance
         """
         return (self.__size ** 2)
-
-    def _tuple_(self, position):
-        """
-        check if it is a tuple and integer
-        """
-        if type(position) is not tuple or len(position) != 2:
-            return False
-        elif type(position[0]) is not int or position[0] < 0:
-            return False
-        elif type(position[1]) is not int or position[1] < 0:
-            return False
-        else:
-            return True
