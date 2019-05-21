d974822fe0699692b76c50f7755745e2d176332b
OrangeB0lt <667@holbertonschool.com>
2019-05-21 19:19:14 -0400

    1 square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Square Class def
+"""
+
+class Square:
+    """
+    Square class with priv instance att size
+    """
+
+    def __init__(self, size=0):
+        """
+        Args:
+          size: size of square
+        """
+        self.__size = size
