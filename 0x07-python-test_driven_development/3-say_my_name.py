adfc129a0ea4c59909d3b8b31efbccd816d30402
OrangeB0lt <667@holbertonschool.com>
2019-05-24 21:05:36 -0400

    initial commit

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Function to print out input name
+"""
+def say_my_name(first_name, last_name=""):
+    """
+        Function that prints input name
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print(first_name, last_name)
