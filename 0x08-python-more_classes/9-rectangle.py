05d63cec539857ef4e6d7bf55c85964f35d7cd94
OrangeB0lt <667@holbertonschool.com>
2019-05-28 15:54:34 -0400

    executable now

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -12,16 +12,18 @@ class Rectangle:
     perimeter
     both have setter and property defs
     """
-    
+    number_of_instances = 0 
+    print_symbol = "#"   
 
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
     
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ("")
-        width = '#' * self.width
+        width = ("{}".format(self.print_symbol)) * self.width
         rectangle = width
         for idx in range(self.height - 1):
             rectangle += "\n" + width
@@ -31,6 +33,7 @@ class Rectangle:
         return("Rectangle({:d}, {:d})".format(self.width, self.height))
 
     def __del__(self):
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
     @property
@@ -68,3 +71,18 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        elif not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        elif rect_1.area() < rect_2.area():
+            return(rect_2)
+        return (rect_1)
+
+    @classmethod
+    def square(cls, size=0):
+        return (cls(size, size))
+        
