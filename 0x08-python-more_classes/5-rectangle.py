4800d00e8fbd41a53a03df392576182a92536b71
OrangeB0lt <667@holbertonschool.com>
2019-05-28 15:37:08 -0400

    initial commit

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+   Class module for Rectangle 
+"""
+
+class Rectangle:
+    """
+    Class attributes
+    height
+    width
+    area
+    perimeter
+    both have setter and property defs
+    """
+    
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+    
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ("")
+        width = '#' * self.width
+        rectangle = width
+        for idx in range(self.height - 1):
+            rectangle += "\n" + width
+        return (rectangle)
+    
+    def __repr__(self):
+        return("Rectangle({:d}, {:d})".format(self.width, self.height))
+
+    def __del__(self):
+        print("Bye rectangle...")
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """<WIDTH> * <HEIGHT> = <AREA>
+        """
+        return self.width * self.height
+    
+    def perimeter(self):
+        """2 * (<WIDTH> + <HEIGHT>)
+        """
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
