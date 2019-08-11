b3b7003468add489a2d45d4e37ef867fe71c13da
OrangeB0lt <667@holbertonschool.com>
2019-08-11 16:56:31 -0400

    init commit for 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' displays states in a CL query '''
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2], argv[3]))
+    Sess = sessionmaker(bind=engine)
+    sess = Sess()
