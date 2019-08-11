24184b24547f09173a8c3b74e8fb74f247a08ec0
OrangeB0lt <667@holbertonschool.com>
2019-08-11 15:31:14 -0400

    init commit for 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' lists all states in DB hbtn_0e_0_usa using SQLAlchemy '''
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2], argv[3]))
+    Sess = sessionmaker(bind=engine)
+    sess = Sess()
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+    session.close()
+    
