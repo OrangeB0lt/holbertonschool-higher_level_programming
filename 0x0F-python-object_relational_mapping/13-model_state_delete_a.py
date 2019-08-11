6a0d29adc942930a253ad5237cb48aa44adb680c
OrangeB0lt <667@holbertonschool.com>
2019-08-11 17:30:49 -0400

    init commit for 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' lists all states from DB using SQLAlchemy '''
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2], argv[3]))
+    Sess = sessionmaker(bind=engine)
+    sess = Sess()
+    for state in sess.query(State).filter(
+            State.name.contains('a')).all():
+        sess.delete(state)
+        sess.commit()
+        sess.close()
+        
