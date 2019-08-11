108f1f55a02f446ce3864ce4c343a7d2ff27b18a
OrangeB0lt <667@holbertonschool.com>
2019-08-11 15:08:06 -0400

    model_state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-"""
-model_state.py: contains class definition of a State
-"""
+'''model_state.py: contains class definition of a State '''
+
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -15,6 +14,5 @@ class State(Base):
     maps to 'states' table in hbtn_0e_6_usa' database
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=Truem nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    
