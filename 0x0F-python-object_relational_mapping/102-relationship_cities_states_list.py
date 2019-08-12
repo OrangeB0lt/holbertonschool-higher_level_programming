3135dd6cfcca88042955d97aa055e0c08bc78216
OrangeB0lt <667@holbertonschool.com>
2019-08-11 21:43:37 -0400

    init commit for 102

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' lists all City objects '''
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from relationship_state import Base, State
+    from relationship_city import City
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Sess = sessionmaker(bind=engine)
+    sess = Sess()
+
+    for city in session.query(City).order_by(City.id).all():
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+    sess.close()
+    
