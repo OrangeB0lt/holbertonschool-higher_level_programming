a3104a882ee7c60ae430b2fffc238f7d964e9520
OrangeB0lt <667@holbertonschool.com>
2019-08-26 17:00:44 -0400

    [Higher lever programming][Python web scripts] Rewrote code, was not connecting correctly

--- 0x11-python-network_1/102-starwars.py
+++ 0x11-python-network_1/102-starwars.py
@@ -3,22 +3,29 @@
 import requests
 from sys import argv
 
-if __name__ == '__main__':
 
-    search = argv[1]
-    page = 1
-    url = 'https://swapi.co/api/people/?search={}&page={}'.format(search, page)
-    json = requests.get(url).json()
-    print('Number of results: {}'.format(json['count']))
-    try:
-        while json.get('count'):
-            for result in json.get('results'):
-                print(result.get('name'))
-                for film in result.get('films'):
-                    print('\t' + requests.get(film).json().get('title'))
-            page += 1
-            url = 'https://swapi.co/api/people/?search={}&page={}'.format(
-                  search, page)
-            json = requests.get(url).json()
-    except Exception:
-        pass
+def request_to_star_wars(the_url, payload):
+    """makes a request to input URL with q as a parameter"""
+    reqst = requests.get(the_url, params=payload).json()
+    name_list = []
+    count = reqst.get('count')
+    if count > 0:
+        results = reqst.get('results')
+        for character in results:
+            name_list.append(character.get('name'))
+    next_page = reqst.get('next')
+    while next_page:
+        reqst = requests.get(next_page).json()
+        results = reqst.get('results')
+        for character in results:
+            name_list.append(character.get('name'))
+        next_page = reqst.get('next')
+    print("Number of results: {}".format(count))
+    for name in name_list:
+        print(name)
+
+if __name__ == "__main__":
+    """MAIN APP"""
+    the_url = "https://swapi.co/api/people/"
+    payload = {'search': sys.argv[1]}
+    request_to_star_wars(the_url, payload)
