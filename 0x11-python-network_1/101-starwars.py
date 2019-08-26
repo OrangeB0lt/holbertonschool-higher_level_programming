a62d1c727d06cbb36af662e3b7aa027d58c7cdb0
OrangeB0lt <667@holbertonschool.com>
2019-08-26 16:58:57 -0400

    [Higher lever programming][Python web scripts] Rewrote code, was not connecting correctly

--- 0x11-python-network_1/101-starwars.py
+++ 0x11-python-network_1/101-starwars.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
 ''' takes in a string, sends a search request to Star Wars API '''
 import requests
-from sys import argv
+import sys
 
 
-if __name__ == '__main__':
+def request_to_star_wars(the_url, payload):
+    """makes a request to input URL with q as a parameter"""
+    reqst = requests.get(the_url, params=payload).json()
+    name_list = []
+    count = reqst.get('count')
+    if count > 0:
+        results = reqst.get('results')
+        for character in results:
+            name_list.append(character.get('name'))
+    next_page = reqst.get('next')
+    while next_page:
+        reqst = requests.get(next_page).json()
+        results = reqst.get('results')
+        for character in results:
+            name_list.append(character.get('name'))
+        next_page = reqst.get('next')
+    print("Number of results: {}".format(count))
+    for name in name_list:
+        print(name)
 
-    search = argv[1]
-    page = 1
-    url = 'https://swapi.co/api/people/?search={}&page={}'.format(search, page)
-    json = requests.get(url).json()
-    print('Number of results: {}'.format(json['count']))
-    try:
-        while json.get('count'):
-            for result in json['results']:
-                print(result['name'])
-            page += 1
-            url = 'https://swapi.co/api/people/?search={}&page={}'.format(
-                  search, page)
-            json = requests.get(url).json()
-    except Exception:
-        pass
+if __name__ == "__main__":
+    """MAIN APP"""
+    the_url = "https://swapi.co/api/people/"
+    payload = {'search': sys.argv[1]}
+    request_to_star_wars(the_url, payload)
