8ddf5165e2ab1c216abb55a32cbd78fdb8d0b887
OrangeB0lt <667@holbertonschool.com>
2019-08-26 16:26:20 -0400

    [Higher lever programming][Python web scripts] Rewrote script 1 with more explicite code

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 ''' displays X-Request-Id in header of response '''
-import urllib.request as request
+import urllib.request
 from sys import argv
 
 
 if __name__ == "__main__":
     URL = argv[1]
     with urllib.request.urlopen(URL) as request:
-        print(response.getheader('X-Request-Id'))
+        print(request.getheader('X-Request-Id'))
